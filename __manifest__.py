fe448627fd19a4fb361aff94261c5d6c3b5313e7
lauri31 <lauri.hans@gmail.com>
2023-01-11 17:18:24 +0700

    cb push

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name' : 'Andika',
+    'version' : '1.0',
+    'summary': 'Andika',
+    'sequence': 10,
+    'description': """
+Andika
+    """,
+    'category': 'Project',
+    'depends' : [
+        'l10n_generic_coa',
+        'stock',
+        'purchase',
+        'mrp',
+        'sale'
+        'account',
+    ],
+    'data': [
+        'data/user.xml',
+    ],
+    'demo': [
+        # 'demo/account_demo.xml',
+    ],
+    'installable': True,
+    # 'application': True,
+  # 'post_init_hook': '_sudestada_init',
+    'license': 'LGPL-3',
+}
+#SDG
